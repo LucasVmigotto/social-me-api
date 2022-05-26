b2e13feb36294bafc8e6c63edee58938e79fcc4e
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-05-26 14:54:26 -0300

    feat(context): add context module
    Create mailer context
    Create logger context
    Create main context

---
+++ src/context/logger.ts
@@ -0,0 +1,18 @@
+import {
+  Logger,
+  createLogger,
+  format,
+  transports
+} from 'winston'
+
+export default (config: any): Logger => {
+  const formatLogger = config.NODE_ENV === 'production'
+    ? format.combine(format.timestamp(), format.json())
+    : format.combine(format.timestamp(), format.prettyPrint(), format.colorize())
+  return createLogger({
+    silent: config.NODE_ENV === 'test',
+    level: config.LOG_LEVEL,
+    format: formatLogger,
+    transports: [new transports.Console()]
+  })
+}
