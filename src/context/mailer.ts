d1d530316ac26d4b38e546e701c371c519f257df
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-05-27 11:51:35 -0300

    feat(context): add mail template plugin to mailer
    Config hbs mail template plugin to mailer

--- src/context/mailer.ts
+++ src/context/mailer.ts
@@ -1,22 +1,41 @@
 
+import {
+  createTransport,
+  SendMailOptions
+} from 'nodemailer'
+import
+  hbs,
+  { TemplateOptions }
+from 'nodemailer-express-handlebars'
+import { resolve } from 'path'
 import { Logger } from 'winston'
-import { createTransport } from 'nodemailer'
+
+export type SendMailOptionsExtented = SendMailOptions & TemplateOptions
 
 export default (config: any, logger: Logger) => {
-
   const mailer = createTransport({
     host: config.EMAIL_HOST,
     port: config.EMAIL_PORT,
-    auth: config.EMAIL_AUTH
+    auth: config.EMAIL_AUTH_USERNAME && config.EMAIL_AUTH_PASSWORD
+      ? { user: config.EMAIL_AUTH_USERNAME, pass: config.EMAIL_AUTH_PASSWORD }
+      : undefined
   })
-
+  const templates = resolve(__dirname, '..', 'templates/')
+  mailer.use(
+    'compile',
+    hbs({
+      viewEngine: {
+        partialsDir: templates,
+        defaultLayout: false
+      },
+      viewPath: templates
+    })
+  )
   mailer.on('error', err => {
     logger.error(err.message)
   })
-
   mailer.on('idle', () => {
     logger.info('Mailer idle')
   })
-
   return mailer
 }
