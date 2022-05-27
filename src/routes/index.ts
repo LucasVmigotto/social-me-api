35ef341bef9251944eba8772e266d6d14e282b21
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-05-26 21:09:45 -0300

    feat(routes): add auth routes
    Add to router function auth endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,17 @@
 import { Response } from 'express'
 import User from '../controllers/user'
+import { authMiddleware } from '../utils'
 
 export default (app: any) => {
   // Auth
   app.patch('/auth/active-account', User.active)
+  app.post('/auth/forgot-password', User.forgotPassword)
+  app.post('/auth/reset-password', User.resetPassword)
+  app.post('/auth/login', User.login)
 
   // Users
   app.post('/users/create', User.create)
+  app.get('/users/me', authMiddleware, User.whoami)
 
   app.use((req: any, res: Response) => res
     .status(404).send({
